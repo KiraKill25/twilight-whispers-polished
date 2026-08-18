bb13650a66faceb944f8cdbd41a86f4be4605b8b
KiraKill25 <45837225+KiraKill25@users.noreply.github.com>
2026-08-18 16:11:08 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       nitro: {
         build: {
           rollupOptions: {
-            input: {},
+            input: "src/entry-client.tsx",
           },
         },
       },
