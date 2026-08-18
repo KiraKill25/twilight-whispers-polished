826835899ea72c6a7c3d93a277fcbcb452f682bd
KiraKill25 <45837225+KiraKill25@users.noreply.github.com>
2026-08-18 14:32:00 +0100

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,19 +1,9 @@
-import { createRootRoute, HeadContent, Scripts, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
   component: RootComponent,
 });
 
 function RootComponent() {
-  return (
-    <html lang="en">
-      <head>
-        <HeadContent />
-      </head>
-      <body>
-        <Outlet />
-        <Scripts />
-      </body>
-    </html>
-  );
+  return <Outlet />;
 }
