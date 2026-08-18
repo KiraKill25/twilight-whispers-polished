c456f5af036012044423da09dc5714d4f4767467
KiraKill25 <45837225+KiraKill25@users.noreply.github.com>
2026-08-18 13:13:09 +0100

    Update start.ts

--- src/start.ts
+++ src/start.ts
@@ -1,12 +1,5 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { RouterProvider } from "@tanstack/react-router";
-import { getRouter } from "./router";
+import { createStart } from "@tanstack/react-start";
 
-const router = getRouter();
-const rootElement = document.getElementById("root");
-
-if (rootElement) {
-  const root = createRoot(rootElement);
-  root.render(React.createElement(RouterProvider, { router }));
-}
+export const startInstance = createStart(() => ({
+  defaultSsr: false,
+}));
